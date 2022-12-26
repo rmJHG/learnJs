9290be13e4382f5b89995b85c89e61cc6eead596
rmJHG <jhg990508@gmail.com>
2022-12-27 02:21:06 +0900

    20221227

--- googleClone/js/weather.js
+++ googleClone/js/weather.js
@@ -1,9 +1,16 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 const API_KEY = "b0cb44432057bee080d3e3db2ed3a68d";
 
-function onGeoOk() {
-  const log = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  console.log(url);
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon= position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+ fetch(url).then((response) => response.json())
+ .then((data) => {
+   city.innerText = data.name;
+   weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+ });
 }
 
 function onGeoError() {
