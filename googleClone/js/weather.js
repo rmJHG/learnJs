7f9b157c26cf72fcb5c7aa62d07b45e7c0357987
rmJHG <jhg990508@gmail.com>
2022-12-27 15:06:10 +0900

    20221227(3)

--- googleClone/js/weather.js
+++ googleClone/js/weather.js
@@ -9,7 +9,7 @@ function onGeoOk(position) {
  fetch(url).then((response) => response.json())
  .then((data) => {
    city.innerText = data.name;
-   weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+   weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
  });
 }
 
